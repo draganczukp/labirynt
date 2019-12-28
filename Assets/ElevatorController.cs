e9b2ba22395aed73d630581f5badfacefb432617
Przemek Draganczuk <draganczukp@gmail.com>
2019-12-28 22:13:46 +0100

    My boss forced me to build this feature... Pure shit.

---
+++ Assets/ElevatorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+
+	enum Going { UP, DOWN };
+
+	public Vector3 bottom;
+	public Vector3 top;
+
+
+	private Going going = Going.UP;
+	private float progress = 0;
+
+    void FixedUpdate()
+    {
+        
+    }
+
+	public void CallElevator(Going going){
+		if(going == Going.DOWN)
+		{
+			if(progress == 1)
+			{
+
+			}
+		}
+	}
+}
